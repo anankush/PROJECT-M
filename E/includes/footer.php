3ef42dfa0062e602676153ec3a00153cd91ee608
Nayan Ghosh <anankush@users.noreply.github.com>
2026-05-30 16:14:39 +0530

    Remove all files from repository

--- E/includes/footer.php
+++
@@ -1,3 +0,0 @@
-    <div class="cursor-glow"></div>
-</body>
-</html>
--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/includes/config.php';
-
-session_unset();
-session_destroy();
-
-header('Location: login.php');
-exit;
